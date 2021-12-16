0c5877ec851434e186fc10cb020202188ce77f43
mhamerlaine <mhamerlaine@synox.io>
2021-12-16 13:07:42 +0100

    .

--- js/form.js
+++ js/form.js
@@ -5,3 +5,16 @@ dans constructor => fb: FormBuilder
 f(): { [key: string]: AbstractControl } {
     return this.form.controls;
 }
+
+<form [formGroup]="form" (ngSubmit)="save()">
+  <!-- Name -->
+  <div>
+    <mat-form-field appearance="legacy">
+      <mat-label translate>Dashboard.Group.ManageOne.GroupName</mat-label>
+      <input matInput type="text" formControlName="name" />
+      <button matSuffix mat-icon-button aria-label="Clear" *ngIf="f().name?.value" (click)="f().name?.reset()">
+        <mat-icon>close</mat-icon>
+      </button>
+    </mat-form-field>
+  </div>
+</form>
